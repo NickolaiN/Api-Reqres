438b5d3a785d58c4fe6a2ffb5f8cbc421b9f560e
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:04:26 +0300

    Update Test06_GetSingleResourceNotFound.java

--- src/test/java/Test06_GetSingleResourceNotFound.java
+++ src/test/java/Test06_GetSingleResourceNotFound.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Test06_GetSingleResourceNotFound {
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+
+public class Test06_GetSingleResourceNotFound extends ResponseFile{
+    @Test
+    @Description("Цвет не найден")
+    public void getSingleResourceNotFOund() throws IOException {
+        Response response = RestAssured.get("https://reqres.in/api/unknown/23");
+
+        System.out.println(response.statusLine());
+        responseMethod(response.asString());
+
+        int statusCode =  response.statusCode();
+        Assert.assertEquals(statusCode,404);
+
+        given().get("https://reqres.in/api/unknown/23")
+                .then()
+                .statusCode(404);
+
+    }
 }
