fc84de50994c65ce275fe8fdaaf57060c1d57ecc
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:06:01 +0300

    Update Test09_PatchUpdate.java

--- src/test/java/Test09_PatchUpdate.java
+++ src/test/java/Test09_PatchUpdate.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Test09_PatchUpdate {
+import io.qameta.allure.Description;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test09_PatchUpdate extends ResponseFile{
+    @Test
+    @Description("Патч тест")
+    public void patchUpdate(){
+        JSONObject request = new JSONObject();
+        request.put("name","oliver");
+
+
+        System.out.println(request);
+
+        given()
+                .body(request.toJSONString())
+                .when().patch("https://reqres.in/api/users/2")
+                .then().statusCode(200);
+    }
 }
