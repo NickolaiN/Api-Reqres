beb20126404f30a65165e405e95b225ad9632535
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:06:54 +0300

    Update Test14_PostLoginUnsuccess.java

--- src/test/java/Test14_PostLoginUnsuccess.java
+++ src/test/java/Test14_PostLoginUnsuccess.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Test14_PostLoginUnsuccess {
+import io.qameta.allure.Description;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test14_PostLoginUnsuccess {
+
+    @Test
+    @Description("Вход (неудачно)")
+    public void postLoginUnsuccess(){
+        JSONObject request = new JSONObject();
+        request.put("email","peter@kelvin");
+
+        System.out.println(request);
+
+        given()
+                .body(request.toJSONString())
+                .when()
+                .header("Content-Type", "application/json")
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .log().body();
+    }
+
 }
