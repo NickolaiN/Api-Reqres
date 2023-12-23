009eece5b0242079b6510361197d896b8efac9e9
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:06:34 +0300

    Update Test12_PostRegisterUnsuccess.java

--- src/test/java/Test12_PostRegisterUnsuccess.java
+++ src/test/java/Test12_PostRegisterUnsuccess.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Test12_PostRegisterUnsuccess {
+import io.qameta.allure.Description;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test12_PostRegisterUnsuccess {
+    @Test
+    @Description("Регистрация (неудачно)")
+    public void postRegisterUnsuccess(){
+        JSONObject request = new JSONObject();
+        request.put("email","testing@mail.in");
+
+        System.out.println(request);
+
+        given()
+                .body(request.toJSONString())
+                .when()
+                .header("Content-Type", "application/json")
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .log().body();
+    }
 }
+
