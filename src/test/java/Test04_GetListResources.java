f5b070f6c36daabdaa14e0ac9afa9af13350328e
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:03:44 +0300

    Update Test04_GetListResources.java

--- src/test/java/Test04_GetListResources.java
+++ src/test/java/Test04_GetListResources.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Test04_GetListResources {
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Test04_GetListResources extends ResponseFile{
+    @Test
+    @Description("Проверка цветов")
+    public void getListResources() throws IOException {
+        Response response = RestAssured.get("https://reqres.in/api/unknown");
+
+        System.out.println(response.statusLine());
+        responseMethod(response.asString());
+
+        int statusCode =  response.statusCode();
+        Assert.assertEquals(statusCode,200);
+
+        given().get("https://reqres.in/api/unknown")
+                .then()
+                .statusCode(200)
+                .body("data.id[0]", equalTo(1));
+
+    }
 }
