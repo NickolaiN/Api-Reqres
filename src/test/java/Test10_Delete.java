3f7d3312fb0192f69e67854d5f996364e5bebbe6
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:06:13 +0300

    Update Test10_Delete.java

--- src/test/java/Test10_Delete.java
+++ src/test/java/Test10_Delete.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Test10_Delete {
+import io.qameta.allure.Description;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test10_Delete{
+    @Test
+    @Description("Удаление")
+    public void delete(){
+        JSONObject request = new JSONObject();
+        given()
+                .body(request.toJSONString())
+                .when().delete("https://reqres.in/api/users/2").then()
+                .statusCode(204).log();
+    }
 }
