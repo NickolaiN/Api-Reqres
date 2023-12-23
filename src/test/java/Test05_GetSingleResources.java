b952866d247889c623c8549f1070e76253cb204e
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:04:16 +0300

    Update Test05_GetSingleResources.java

--- src/test/java/Test05_GetSingleResources.java
+++ src/test/java/Test05_GetSingleResources.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Test05_GetSingleResources {
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Test05_GetSingleResources extends ResponseFile{
+    @Test
+    @Description("Проверка цвета")
+    public void getSingleResource() throws IOException {
+        Response response = RestAssured.get("https://reqres.in/api/unknown/2");
+
+        System.out.println(response.statusLine());
+        responseMethod(response.asString());
+
+        int statusCode =  response.statusCode();
+        Assert.assertEquals(statusCode,200);
+
+        given().get("https://reqres.in/api/unknown/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", equalTo(2));
+
+    }
 }
+
