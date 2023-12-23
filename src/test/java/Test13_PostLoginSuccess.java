9df5586276d8ffbc9d76c456bddcb189d856c0ef
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:06:45 +0300

    Update Test13_PostLoginSuccess.java

--- src/test/java/Test13_PostLoginSuccess.java
+++ src/test/java/Test13_PostLoginSuccess.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Test13_PostLoginSuccess {
+import io.qameta.allure.Description;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test13_PostLoginSuccess {
+
+    @Test
+    @Description("Вход")
+    public void postLoginSuccess(){
+        JSONObject request = new JSONObject();
+        request.put("email","eve.holt@reqres.in");
+        request.put("password","testPass");
+
+        given()
+                .body(request.toJSONString())
+                .when()
+                .header("Content-Type", "application/json")
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .log().body();
+    }
 }
