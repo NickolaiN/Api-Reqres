89b8567e4e6e2676bde98b3f20e9b74475a1e39b
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:03:13 +0300

    Update Test02_GetSingleUser.java

--- src/test/java/Test02_GetSingleUser.java
+++ src/test/java/Test02_GetSingleUser.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Test02_GetSingleUser {
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Test02_GetSingleUser extends ResponseFile{
+    @Test
+    @Description("Проверка пользователя")
+    public void singleUser() throws IOException {
+        Response response = RestAssured.get("https://reqres.in/api/users/2");
+        System.out.println(response.statusLine());
+
+        responseMethod(response.asString());
+
+        int statusCode =  response.statusCode();
+        Assert.assertEquals(statusCode,200);
+
+        given().get("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", equalTo(2));
+    }
 }
