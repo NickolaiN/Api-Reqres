108b9e1def13e0f0ac1a6dd1f6e5aa02595bcf55
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:05:28 +0300

    Update Test07_PostCreate.java

--- src/test/java/Test07_PostCreate.java
+++ src/test/java/Test07_PostCreate.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Test07_PostCreate {
+import io.qameta.allure.Description;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Test07_PostCreate {
+    @Test
+    @Description("Проверка после создания пользователя")
+    public void create(){
+        JSONObject request = new JSONObject();
+        request.put("name","JOHN");
+        request.put("job","BA");
+
+        System.out.println(request);
+
+        given().
+                body(request.toJSONString())
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201);
+    }
 }
