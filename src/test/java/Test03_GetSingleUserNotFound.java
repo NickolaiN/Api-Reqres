1a808204026a207b6307404b4f77e276120cc2dc
Николай Н. А <147653381+NickolaiN@users.noreply.github.com>
2023-12-23 03:03:29 +0300

    Update Test03_GetSingleUserNotFound.java

--- src/test/java/Test03_GetSingleUserNotFound.java
+++ src/test/java/Test03_GetSingleUserNotFound.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class est03_GetSingleUserNotFound {
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+
+
+public class Test03_GetSingleUserNotFound extends ResponseFile{
+
+    @Test
+    @Description("Проверка пользователя (не найден)")
+    public void userNotFound() throws IOException {
+        Response response = RestAssured.get("https://reqres.in/api/users/23");
+
+        System.out.println(response.statusLine());
+        responseMethod(response.asString());
+
+        int statusCode =  response.statusCode();
+        Assert.assertEquals(statusCode,404);
+
+        given().get("https://reqres.in/api/users/23")
+                .then()
+                .statusCode(404);
+
+    }
 }
